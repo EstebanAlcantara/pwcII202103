d01c774dcb49ab2e73b478a61d344bf9ffc44d81
EstebanAlcantara <arcticdead@gmail.com>
2021-04-15 10:52:20 -0500

    agregando el nombre

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author:'Esteban Torres' });
+  res.render('index', { title: 'Express', author:'Esteban Torres', comapany: 'CompuMundo' });
 });
 
 router.get('/itgam', function(req, res, next) {
